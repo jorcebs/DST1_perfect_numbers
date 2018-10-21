cf4f4c250a4affcb3d984635edbbe76f90a27024
jorcebs <jor.cebs@gmail.com>
2018-10-20 19:54:17 -0500

    Upload

---
+++ Números perfectos/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Números_perfectos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.lstNúmeros = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.AutoSize = true;
+            this.btnMostrar.Location = new System.Drawing.Point(158, 72);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(113, 34);
+            this.btnMostrar.TabIndex = 1;
+            this.btnMostrar.Text = "Mostrar";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // lstNúmeros
+            // 
+            this.lstNúmeros.FormattingEnabled = true;
+            this.lstNúmeros.ItemHeight = 24;
+            this.lstNúmeros.Location = new System.Drawing.Point(12, 12);
+            this.lstNúmeros.Name = "lstNúmeros";
+            this.lstNúmeros.Size = new System.Drawing.Size(91, 340);
+            this.lstNúmeros.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(331, 380);
+            this.Controls.Add(this.lstNúmeros);
+            this.Controls.Add(this.btnMostrar);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "Números perfectos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.ListBox lstNúmeros;
+    }
+}
+
---
+++ Números perfectos/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Números_perfectos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            int suma = 0;
+            for (int num = 9999; num > 0; num--)
+            {
+                for (int i = num/2; i > 0; i--)
+                    if (num % i == 0)
+                        suma += i;
+                if (suma == num)
+                    lstNúmeros.Items.Add(num);
+                suma = 0;
+            }
+        }
+    }
+}
